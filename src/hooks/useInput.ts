0c8ecbf20e6ae52bf1109e62d48767a1d2b24677
jejecrunch <gkswlgp456@gmail.com>
2024-02-06 17:47:38 +0900

    fix: any 오류 해당 내용 삭제 하여 해결

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 type UseInputParam = {
   initVal: string | '';
-  validation: (v: string) => boolean | any | never;
+  validation: (v: string) => boolean | never;
 };
 
 export default function useInput({ initVal, validation }: UseInputParam) {
@@ -24,3 +24,40 @@ export default function useInput({ initVal, validation }: UseInputParam) {
 
   return { value, onChange, valid, onReset };
 }
+
+// import { useState } from 'react';
+
+// type UseInputReturn = {
+//   value: string | '';
+//   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+//   valid: boolean;
+//   onReset: () => void;
+// };
+
+// type UseInputParam = {
+//   initVal: string | '';
+//   validation: (v: string) => boolean | any | never;
+// };
+
+// export default function useInput({
+//   initVal,
+//   validation,
+// }: UseInputParam): UseInputReturn {
+//   const [value, setValue] = useState(initVal);
+//   const [valid, setValid] = useState(false);
+
+//   function onChange(e: React.ChangeEvent<HTMLInputElement>) {
+//     const {
+//       target: { value },
+//     } = e;
+
+//     setValue(value);
+//     setValid(validation(value));
+//   }
+
+//   function onReset() {
+//     setValue('');
+//   }
+
+//   return { value, onChange, valid, onReset };
+// }
